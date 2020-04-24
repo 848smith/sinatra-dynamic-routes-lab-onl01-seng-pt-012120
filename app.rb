54f2032ddb182ad4f0b30895009985da9f1f12e8
Andrew Smith <848smith@gmail.com>
2020-04-24 02:37:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,4 +4,16 @@ class App < Sinatra::Base
   get '/reversename/:name' do
     params[:name].reverse
   end
+  
+  get '/square/:number' do
+    params[:number].to_i
+  end
+  
+  get '/say/:number/:phrase' do
+    
+  end
+  
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    
+  end
 end
